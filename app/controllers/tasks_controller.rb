78ca375a13e0e785f405fa312d25409afbe508d5
Théo Dufour <theo.dufthir@gmail.com>
2023-03-28 17:33:48 +0200

    Added Tasks endpoints without verification

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  def get_params
+    params.require(:task).permit(:title, :description, :done, :author, :start, :end)
+  end
+
+  public
+  def create
+    @todo = Task.new(get_params)
+    if @todo.save
+      render json: @todo
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @todos = Task.all
+    render json: @todos
+  end
+
+  def show
+    @todo = Task.find(params[:id])
+    render json: @todo
+  end
+
+  def destroy
+    @todo = Task.find(params[:id])
+    @todo.destroy
+    render json: @todo
+  end
+
+  def update
+    @todo = Task.find(params[:id])
+
+    if !@todo.update(get_params)
+      render json: @todo
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
