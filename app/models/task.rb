8088efdd0b7205b9e67817baa91da9da9eef6ff5
Theodufthir <theo.dufthir@gmail.com>
2023-04-06 19:58:20 +0200

    Added links to Task and User ruby models, automatically linking the tasks and users for each object

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+  belongs_to :user, foreign_key: :author_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   self.primary_key = :auth0Id
+  has_many :tasks, primary_key: :auth0Id, foreign_key: :author_id
 end
