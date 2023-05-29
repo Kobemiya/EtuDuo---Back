c222dd6333704c71ff90efd80a472e94f6680c07
Theodufthir <theo.dufthir@gmail.com>
2023-05-29 12:54:53 +0200

    Added cascade delete and nullify delete cause I forgor

--- db/migrate/20230525145700_create_companion.rb
+++ db/migrate/20230525145700_create_companion.rb
@@ -3,14 +3,14 @@ class CreateCompanion < ActiveRecord::Migration[7.0]
     create_table :companions do |t|
       t.string :name, null: false
       t.string :skin_color, null: false
-      t.references :face, foreign_key: { to_table: :accessories }
-      t.references :hands, foreign_key: { to_table: :accessories }
-      t.references :hair, foreign_key: { to_table: :accessories }
-      t.references :neck, foreign_key: { to_table: :accessories }
-      t.references :torso, foreign_key: { to_table: :accessories }
-      t.references :legs, foreign_key: { to_table: :accessories }
-      t.references :feet, foreign_key: { to_table: :accessories }
-      t.references :user, type: :string, null: false, foreign_key: { to_table: :users, primary_key: :auth0Id }
+      t.references :face, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :hands, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :hair, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :neck, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :torso, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :legs, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :feet, foreign_key: { to_table: :accessories, on_delete: :nullify }
+      t.references :user, type: :string, null: false, foreign_key: { to_table: :users, primary_key: :auth0Id, on_delete: :cascade }
 
       t.timestamps
     end
