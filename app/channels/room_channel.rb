d44b9a5f6071f94afda38399c24a48b4670720f2
Theodufthir <theo.dufthir@gmail.com>
2023-10-20 03:59:39 +0200

    Added room status message when users subscribe/unsubscribe to a room's feed

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,23 +1,52 @@
 class RoomChannel < ApplicationCable::Channel
   private
 
-  def broadcast_message(data)
-    ActionCable.server.broadcast "room_#{params[:room_id]}", {
+  @@subscribers = {}
+
+  after_subscribe :broadcast_room_status_callback
+  after_unsubscribe :broadcast_room_status_callback
+
+  def broadcast_message(room_id, data)
+    ActionCable.server.broadcast "room_#{room_id}", {
       type: "chat_message",
       user_id: connection.user.auth0Id,
       content: data
     }
   end
 
+  def broadcast_room_status_callback
+    sleep(0.1)  # absolutely magnificent fix
+    broadcast_room_status(params[:room_id])
+  end
+
+  def broadcast_room_status(room_id)
+    ActionCable.server.broadcast "room_#{room_id}", {
+      type: "room_status",
+      users_connected: @@subscribers[room_id]
+    }
+  end
+
   public
 
   def subscribed
-    return reject unless connection.user.joined_rooms.exists?(params[:room_id])
-    stream_from "room_#{params[:room_id]}"
-    @room = Room.find(params[:room_id])
+    room_id = params[:room_id]
+    user = connection.user
+    return reject unless user.joined_rooms.exists?(room_id)
+    return reject if @@subscribers.has_key?(room_id) && @@subscribers[room_id].any?(user.auth0Id)
+    stream_from "room_#{room_id}"
+    @@subscribers[room_id] = [] unless @@subscribers.has_key?(room_id)
+    @@subscribers[room_id].append(user.auth0Id)
   end
 
+  def unsubscribed
+    room_id = params[:room_id]
+    return @@subscribers.delete(room_id) if @@subscribers[room_id].size == 1
+    @@subscribers[room_id].delete(connection.user.auth0Id)
+  end
+
+
   def receive(data)
-    broadcast_message(data["content"]) if (data["type"] == "chat_message")
+    room_id = params[:room_id]
+    broadcast_message(room_id, data["content"]) if (data["type"] == "chat_message")
   end
 end
