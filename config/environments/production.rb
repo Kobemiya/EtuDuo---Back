b79faa3d20138acc85ea977b37a18be8104fbd69
Theodufthir <theo.dufthir@gmail.com>
2023-10-30 11:54:55 +0100

    Added prod url in allowed_request_origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_cable.allow_request_origins = [/https?:\/etuduo-backend.herokuapp.com.*/]
+  config.action_cable.allow_request_origins = [/https?:\/\/etuduo-backend.herokuapp.com.*/]
 end
