0b5bf425dea5bd0aaec6d519ee083a8db1cad244
theodufthir <theo.dufthir@gmail.com>
2023-12-07 19:28:19 +0100

    Fix Room model (users and user_count were still present)

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,11 +1,8 @@
 # frozen_string_literal: true
 
 class Room < ApplicationRecord
-  belongs_to :user, foreign_key: :author_id
-
   def as_json(options = nil)
     json = super
-    json['user_count'] = users.length
     json['needs_password'] = json['password'].present?
     json.except('author_id', 'password')
   end
